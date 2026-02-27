91ea92b2edfb5e68529b1dc81985e3f9bac5a469
divyadharshini165 <vijayalakshmi3181@gmail.com>
2026-02-27 19:27:14 +0530

    UC11: Object-Oriented palindrome service using encapsulation and SRP

---
+++ src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+// Service class (Encapsulation)
+class PalindromeChecker {
+
+    // Public method to check palindrome
+    public boolean checkPalindrome(String input) {
+
+        if (input == null) {
+            return false;
+        }
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+// Main Application Class
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Create service object
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(input);
+
+        if (result) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
